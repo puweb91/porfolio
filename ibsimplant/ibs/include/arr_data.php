d3f5b961d7c6d8db51ce763766e1f6f0b555a95c
puweb91 <puweb91@gmail.com>
2022-03-31 03:52:52 +0900

    base 업로드

---
+++ ibsimplant/ibs/include/arr_data.php
@@ -0,0 +1,115 @@
+<?
+
+$site_title = ""; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "주문현황";
+
+$nav_2 = "회원관리";
+$nav_2_1 = "의료인";
+$nav_2_2 = "기공소";
+$nav_2_3 = "임직원";
+$nav_2_4 = "마일리지 지급/차감";
+
+$nav_3 = "관리자관리";
+$nav_3_1 = "관리자관리";
+$nav_3_2 = "탈퇴관리자관리";
+
+$nav_4 = "팝업관리";
+$nav_4_1 = "IBS 소개";
+$nav_4_2 = "주문 및 고객센터";
+
+$nav_5 = "고객센터";
+$nav_5_1 = "지점안내";
+$nav_5_2 = "온라인 고객지원";
+$nav_5_3 = "구매문의";
+
+$nav_6 = "Template";
+$nav_6_1 = "list";
+$nav_6_2 = "view";
+$nav_6_3 = "write";
+$nav_6_4 = "buttons";
+$nav_6_5 = "etc";
+
+$nav_7 = "회원정보";
+$nav_7_1 = "회원정보 수정";
+$nav_7_2 = "마일리지 내역";
+$nav_7_3 = "회원탈퇴";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+$nav_member_login = "";
+$nav_member_join = "";
+$nav_member_modify = "내정보";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../order/list.html";
+
+$nav_2_href = "../member/dentist.html";
+$nav_2_1_href = "../member/dentist.html";
+$nav_2_2_href = "../member/lab.html";
+$nav_2_3_href = "../member/employees.html";
+$nav_2_4_href = "../member/mileage.html";
+
+$nav_3_href = "../setting/admin.html";
+$nav_3_1_href = "../setting/admin.html";
+$nav_3_2_href = "../setting/withdrawal_admin.html";
+
+$nav_4_href = "../popup/ibs.html";
+$nav_4_1_href = "../popup/ibs.html";
+$nav_4_2_href = "../popup/order-cs.html";
+
+$nav_5_href = "../cs/branch.html";
+$nav_5_1_href = "../cs/branch.html";
+$nav_5_2_href = "../cs/online-cs.html";
+$nav_5_3_href = "../cs/purchase-inquiry.html";
+
+
+$nav_6_href = "../template/list.html";
+$nav_6_1_href = "../template/list.html";
+$nav_6_2_href = "../template/view.html";
+$nav_6_3_href = "../template/write.html";
+$nav_6_4_href = "../template/buttons.html";
+$nav_6_5_href = "../template/etc.html";
+
+
+$nav_7_href = "../mypage/modify.html";
+$nav_7_1_href = "../mypage/modify.html";
+$nav_7_2_href = "../mypage/mileage.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
